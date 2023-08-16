7215eed8d7ad37c48bde6c89d5772aeed2405fe9
0n0w1c <47831850+0n0w1c@users.noreply.github.com>
2023-08-16 18:23:07 -0500

    Remove cargo-ships

--- data-updates.lua
+++ data-updates.lua
@@ -1,18 +1,5 @@
-local effects = {
-    type = "unlock-recipe",
-    recipe = "fish-pole"
-}
-
--- insert the recipe effect to unlock
-if mods['cargo-ships'] then
-    table.insert(data.raw['technology']['oversea-energy-distribution'].effects, effects)
-    data.raw['recipe']['floating-electric-pole'].subgroup = 'energy-pipe-distribution'
-    if data.raw['technology']['oversea-energy-distribution'].researched then
-        data.raw['recipe']['fish-pole'].enabled = true
-    end
-else
-    table.insert(data.raw['technology']['electric-energy-distribution-1'].effects, effects)
-    if data.raw['technology']['electric-energy-distribution-1'].researched then
-        data.raw['recipe']['fish-pole'].enabled = true
-    end
+-- insert the recipe to unlock
+table.insert(data.raw['technology']['electric-energy-distribution-1'].effects, {type = "unlock-recipe", recipe = "fish-pole"})
+if data.raw['technology']['electric-energy-distribution-1'].researched then
+    data.raw['recipe']['fish-pole'].enabled = true
 end
